693dac921ee26e2d4e50045c2ffde50dd56d87ec
szboynono <szboylbd@hotmail.com>
2021-11-22 19:40:18 -0500

    feat: setup router

---
+++ src/router.ts
@@ -0,0 +1,23 @@
+
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './pages/Home.vue';
+import Details from './pages/Details.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+      path: '/details/:id',
+      name: 'details',
+      component: Details,
+  },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+export default router
